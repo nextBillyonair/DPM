63751b5172161f837880069a06330d6d97454580
nextBillyonair <wwatso13@jhu.edu>
2019-07-04 15:57:05 -0400

    Add LSGAN, rename GANS

--- loss.py
+++ loss.py
@@ -35,7 +35,7 @@ class JSDivergence(Module):
                       + self._forward_kl(q_model, M, batch_size))
 
 
-class JSDAdversarialLoss(Module):
+class GANAdversarialLoss(Module):
     def __init__(self, n_dims, hidden_size=4):
         super().__init__()
         self.n_dims = n_dims
@@ -56,7 +56,7 @@ class JSDAdversarialLoss(Module):
 
         q_samples = q_model.sample(batch_size)
         q_values = self.discriminator_model(q_samples)
-        q_loss = self.bce_loss(q_values, torch.zeros_like(q_values))
+        q_loss = self.bce_loss(q_values, torch.zeros_like(p_values))
 
         loss = 0.5*(p_loss + q_loss)
         loss.backward()
@@ -64,11 +64,11 @@ class JSDAdversarialLoss(Module):
 
         q_samples = q_model.sample(batch_size)
         q_values = self.discriminator_model(q_samples)
-        q_loss = self.bce_loss(q_values, torch.ones_like(q_values))
+        q_loss = self.bce_loss(q_values, torch.ones_like(p_values))
 
         return q_loss
-    
-class EMDAdversarialLoss(Module):
+
+class WGANAdversarialLoss(Module):
     def __init__(self, n_dims, hidden_size=4):
         super().__init__()
         self.n_dims = n_dims
@@ -97,3 +97,33 @@ class EMDAdversarialLoss(Module):
 
         return q_loss
 
+class LSGANAdversarialLoss(Module):
+    def __init__(self, n_dims, hidden_size=4):
+        super().__init__()
+        self.n_dims = n_dims
+        self.hidden_size = hidden_size
+        self.discriminator_model = Sequential(Linear(n_dims, hidden_size),
+                                              LeakyReLU(),
+                                              Linear(hidden_size, 1))
+        self.d_optimizer = optim.RMSprop(self.discriminator_model.parameters(),
+                                         lr=1e-3)
+
+    def forward(self, p_model, q_model, batch_size=64):
+        self.d_optimizer.zero_grad()
+
+        p_samples = p_model.sample(batch_size)
+        p_loss = (self.discriminator_model(p_samples) \
+                  - torch.ones(batch_size)).pow(2).mean()
+
+        q_samples = q_model.sample(batch_size)
+        q_loss = (self.discriminator_model(q_samples)).pow(2).mean()
+
+        loss = 0.5*(p_loss + q_loss)
+        loss.backward()
+        self.d_optimizer.step()
+
+        q_samples = q_model.sample(batch_size)
+        q_loss = 0.5*(self.discriminator_model(q_samples) \
+                     - torch.ones(batch_size)).pow(2).mean()
+
+        return q_loss
