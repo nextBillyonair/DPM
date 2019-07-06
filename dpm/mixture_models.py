5ad04c026c4f2faf28f6427b96462a21fa95e317
nextBillyonair <wwatso13@jhu.edu>
2019-07-06 12:52:46 -0400

    Add convolution, outline learnable chi square

--- dpm/mixture_models.py
+++ dpm/mixture_models.py
@@ -9,7 +9,7 @@ from dpm.distributions import Distribution, GumbelSoftmax, Gamma, Normal
 # Non-differentiable Categorical weights (not learnable)
 class MixtureModel(Distribution):
     def __init__(self, models, probs):
-        super(MixtureModel, self).__init__()
+        super().__init__()
         self.n_dims = models[0].n_dims
         self.categorical = Categorical(probs=torch.tensor(probs))
         self.models = ModuleList(models)
@@ -30,10 +30,41 @@ class MixtureModel(Distribution):
                 'models': [model.get_parameters() for model in self.models]}
 
 
+class Convolution(Distribution):
+    def __init__(self, models):
+        super().__init__()
+        self.n_dims = models[0].n_dims
+        self.models = ModuleList(models)
+        self.n_models = len(models)
+
+    def log_prob(self, value):
+        raise NotImplementedError("Convolution log_prob not implemented")
+
+    def sample(self, batch_size):
+        samples = torch.stack([sub_model.sample(batch_size)
+                               for sub_model in self.models])
+        return samples.sum(0)
+
+    def get_parameters(self):
+        return {'models':[model.get_parameters() for model in self.models]}
+
+
+class ChiSquare(Distribution):
+
+    def __init__(self, df):
+        pass
+
+    def log_prob(self, value):
+        raise NotImplementedError("Chi2 log_prob not implemented")
+
+    def sample(self, batch_size):
+        raise NotImplementedError("Chi2 log_prob not implemented")
+
+
 # Differentiable, Learnable Mixture Weights
 class GumbelMixtureModel(Distribution):
     def __init__(self, models, probs, temperature=1.0, hard=True):
-        super(GumbelMixtureModel, self).__init__()
+        super().__init__()
         self.n_dims = models[0].n_dims
         self.categorical = GumbelSoftmax(probs, temperature, hard)
         self.models = ModuleList(models)
