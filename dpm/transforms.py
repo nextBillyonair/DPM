2c6edc6aa22ba3b14db8973ac53da802434cded0
nextBillyonair <wwatso13@jhu.edu>
2019-07-07 00:32:25 -0400

    Shift TD to distrigbutions

--- dpm/transforms.py
+++ dpm/transforms.py
@@ -1,39 +1,9 @@
 from abc import abstractmethod, ABC
 import math
 import torch
-from torch.nn import Module, Parameter, ModuleList
+from torch.nn import Module, Parameter
 from torch.nn.functional import softplus
-from dpm.distributions import Distribution
 import numpy as np
-
-
-class TransformDistribution(Distribution):
-
-    def __init__(self, distribution, transforms):
-        super().__init__()
-        self.n_dims = distribution.n_dims
-        self.distribution = distribution
-        self.transforms = ModuleList(transforms)
-
-    def log_prob(self, value):
-        prev_value = value
-        log_det = 0.0
-        for transform in self.transforms[::-1]:
-            value = transform.inverse(prev_value)
-            log_det += transform.log_abs_det_jacobian(value, prev_value)
-            prev_value = value
-        return -log_det.sum(1) + self.distribution.log_prob(value)
-
-    def sample(self, batch_size):
-        samples = self.distribution.sample(batch_size)
-        for transform in self.transforms:
-            samples = transform(samples)
-        return samples
-
-    def get_parameters(self):
-        return {'distribution':self.distribution.get_parameters(),
-                'transforms': [transform.get_parameters()
-                               for transform in self.transforms]}
 
 
 class Transform(ABC, Module):
