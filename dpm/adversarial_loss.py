b59d2be99f87d1978e2e349ab0531936aa35fcfa
nextBillyonair <wwatso13@jhu.edu>
2019-07-28 11:36:35 -0400

    Add WGAN + GP to adversarial losses

--- dpm/adversarial_loss.py
+++ dpm/adversarial_loss.py
@@ -5,12 +5,13 @@ from torch.nn import (
     Module, Sequential, Linear, LeakyReLU,
     BCEWithLogitsLoss, MSELoss
 )
+import dpm.utils as utils
 
 class AdversarialLoss(ABC, Module):
 
     def __init__(self, input_dim, hidden_sizes=[24, 24],
                  activation='LeakyReLU',
-                 lr=1e-3):
+                 lr=1e-3, gp_penalty=10.):
         super().__init__()
         self.n_dims = input_dim
         prev_size = input_dim
@@ -23,6 +24,7 @@ class AdversarialLoss(ABC, Module):
         self.discriminator_model = nn.Sequential(*layers)
         self.optimizer = optim.RMSprop(self.discriminator_model.parameters(),
                                        lr=lr)
+        self.gp_penalty = gp_penalty
 
     @abstractmethod
     def discriminator_loss(self, p_values, q_values):
@@ -42,6 +44,9 @@ class AdversarialLoss(ABC, Module):
         q_values = self.discriminator_model(q_samples.detach())
 
         d_loss = self.discriminator_loss(p_values, q_values)
+        if 'calculate_gp' in dir(self):
+            d_loss += self.calculate_gp(p_samples.detach(), q_samples.detach())
+
         d_loss.backward()
         self.optimizer.step()
 
@@ -87,6 +92,18 @@ class WGANLoss(AdversarialLoss):
 
     def generator_loss(self, q_values):
         return q_values.mean()
+
+
+class WGANGPLoss(WGANLoss):
+
+    def calculate_gp(self, p_samples, q_samples):
+        eps = torch.rand((p_samples.shape))
+        x_hat = eps * p_samples + (1 - eps) * q_samples
+        x_hat.requires_grad = True
+        x_hat_values = self.discriminator_model(x_hat).mean()
+        gp = utils.gradient(x_hat_values, x_hat)
+        penalty = self.gp_penalty * (torch.norm(gp, p=2) - 1).pow(2)
+        return penalty
 
 
 class LSGANLoss(AdversarialLoss):
