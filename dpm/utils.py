c3cf50e2048f41baa2bde99333dfddf853d619c4
nextBillyonair <wwatso13@jhu.edu>
2019-08-02 22:19:14 -0400

    Add cov, corr

--- dpm/utils.py
+++ dpm/utils.py
@@ -54,5 +54,25 @@ def pinverse(X):
     return torch.pinverse(X.reshape(rows, rows)).reshape(original)
 
 
+def cov(X, Y=None):
+    if Y is None:
+        Y = X
+    Xm = X - X.mean(dim=0).expand_as(X)
+    Ym = Y - Y.mean(dim=0).expand_as(Y)
+    Xm, Ym = Xm.t(), Ym.t()
+    C = Xm.mm(Ym.t()) / X.size(0)
+    return C
+
+
+def corr(X, Y=None):
+    if Y is None:
+        Y = X
+    C = cov(X, Y)
+    std_x = X.std(dim=0)
+    std_y = Y.std(dim=0)
+    C = C / std_x.expand_as(C)
+    C = C / std_y.expand_as(C).t()
+    return C
+
 
 # EOF
