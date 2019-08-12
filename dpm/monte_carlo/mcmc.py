373de28b240a6c77bc4c5401efd327f62f20db9e
Bill Watson <billy_watson96@yahoo.com>
2019-08-12 11:01:39 -0400

    x->q, v->p, rework HMC slide equations

--- dpm/monte_carlo/mcmc.py
+++ dpm/monte_carlo/mcmc.py
@@ -57,41 +57,41 @@ def grad_U(q, model):
 def hamiltonian_monte_carlo(model, epsilon=0.2, leapfrog=20, epochs=1000,
                             burn_in=1000, keep_every=1, init=None):
     if init is None:
-        current_x = torch.rand((1, model.n_dims))
+        current_q = torch.rand((1, model.n_dims))
     else:
-        current_x = init
-        if not isinstance(current_x, torch.Tensor):
-            current_x = torch.tensor(current_x).view(1, -1)
+        current_q = init
+        if not isinstance(current_q, torch.Tensor):
+            current_q = torch.tensor(current_q).view(1, -1)
 
     samples = []
 
     for t in range(epochs):
-        x = current_x.clone()
-        v = torch.randn((x.size(1), 1))
-        current_v = v.clone()
+        q = current_q.clone()
+        p = torch.randn((q.size(1), 1))
+        current_p = p.clone()
 
-        v = v - epsilon * grad_U(x, model) / 2.
+        p = p - epsilon * grad_U(q, model) / 2.
 
         for l in range(leapfrog):
-            x = x + epsilon * v
+            q = q + epsilon * p
             if l + 1 != leapfrog:
-                v = v - epsilon * grad_U(x, model)
+                p = p - epsilon * grad_U(q, model)
 
-        v = v - epsilon * grad_U(x, model) / 2.
-        v = -v
+        p = p - epsilon * grad_U(q, model) / 2.
+        p = -p
 
-        current_U = -model.log_prob(current_x)
-        current_K = current_v.pow(2).sum() / 2.
-        proposed_U = -model.log_prob(x)
-        proposed_K = v.pow(2).sum() / 2.
+        current_U = -model.log_prob(current_q)
+        current_K = current_p.pow(2).sum() / 2.
+        proposed_U = -model.log_prob(q)
+        proposed_K = p.pow(2).sum() / 2.
 
         u = torch.rand(1).log()
         A = current_U - proposed_U + current_K - proposed_K
         if u < A:
-            current_x = x
+            current_q = q
 
         if t >= burn_in and t % keep_every == 0:
-            samples.append(current_x)
+            samples.append(current_q)
 
     return torch.cat(samples, dim=0)
 
