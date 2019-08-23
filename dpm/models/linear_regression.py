fb0942ffd9e8ddba589e692177c1b0eaa31c1074
nextBillyonair <wwatso13@jhu.edu>
2019-08-22 23:50:54 -0400

    Refactor regression models to inherit from a linear model

--- dpm/models/linear_regression.py
+++ dpm/models/linear_regression.py
@@ -3,74 +3,57 @@ from functools import partial
 from dpm.distributions import Distribution, Normal, ConditionalModel, Laplace
 from .model import Model
 
-class LinearRegression(Model):
+class LinearModel(Model):
 
-    def __init__(self, input_dim=1, output_shape=1):
+    def __init__(self, input_dim=1, output_shape=1, distribution=None, prior=None):
         super().__init__()
         self.model = ConditionalModel(input_dim, hidden_sizes=[], activation="",
                             output_shapes=[output_shape],
                             output_activations=[None],
-                            distribution=partial(Normal, scale=torch.ones(1, output_shape)))
+                            distribution=distribution)
+        self.prior = prior
+
+    def prior_penalty(self):
+        return self.prior.log_prob(torch.cat([p.view(-1) for p in self.model.parameters()])).sum()
 
     def log_prob(self, x, y):
+        if self.prior:
+            return self.model.log_prob(y, x) + self.prior_penalty()
         return self.model.log_prob(y, x)
 
     def sample(self, x, compute_logprob=False):
         return self.model.sample(x, compute_logprob)
 
 
-class L1Regression(Model):
+class LinearRegression(LinearModel):
 
     def __init__(self, input_dim=1, output_shape=1):
-        super().__init__()
-        self.model = ConditionalModel(input_dim, hidden_sizes=[], activation="",
-                            output_shapes=[output_shape],
-                            output_activations=[None],
-                            distribution=partial(Laplace, scale=torch.ones(output_shape)))
+        super().__init__(input_dim, output_shape,
+            distribution=partial(Normal, scale=torch.ones(1, output_shape)))
 
 
-    def log_prob(self, x, y):
-        return self.model.log_prob(y, x)
+class L1Regression(LinearModel):
 
-    def sample(self, x, compute_logprob=False):
-        return self.model.sample(x, compute_logprob)
+    def __init__(self, input_dim=1, output_shape=1):
+        super().__init__(input_dim, output_shape,
+            distribution=partial(Laplace, scale=torch.ones(output_shape)))
 
 
-class RidgeRegression(Model):
+class RidgeRegression(LinearModel):
 
     def __init__(self, input_dim=1, output_shape=1, tau=1.):
-        super().__init__()
-        self.model = ConditionalModel(input_dim, hidden_sizes=[], activation="",
-                            output_shapes=[output_shape],
-                            output_activations=[None],
-                            distribution=partial(Normal, scale=torch.ones(1, output_shape)))
-        self.prior = Normal(0., tau)
-
-    def prior_penalty(self):
-        return self.prior.log_prob(torch.cat([p.view(-1) for p in self.model.parameters()])).sum()
-
-    def log_prob(self, x, y):
-        return self.model.log_prob(y, x) + self.prior_penalty()
-
-    def sample(self, x, compute_logprob=False):
-        return self.model.sample(x, compute_logprob)
+        super().__init__(input_dim, output_shape,
+            distribution=partial(Normal, scale=torch.ones(1, output_shape)),
+            prior=Normal(0., tau))
 
 
-class LassoRegression(Model):
+class LassoRegression(LinearModel):
 
     def __init__(self, input_dim=1, output_shape=1, tau=1.):
-        super().__init__()
-        self.model = ConditionalModel(input_dim, hidden_sizes=[], activation="",
-                            output_shapes=[output_shape],
-                            output_activations=[None],
-                            distribution=partial(Normal, scale=torch.ones(1, output_shape)))
-        self.prior = Laplace(0., tau)
+        super().__init__(input_dim, output_shape,
+            distribution=partial(Normal, scale=torch.ones(1, output_shape)),
+            prior=Laplace(0., tau))
 
-    def prior_penalty(self):
-        return self.prior.log_prob(torch.cat([p.view(-1) for p in self.model.parameters()])).sum()
 
-    def log_prob(self, x, y):
-        return self.model.log_prob(y, x) + self.prior_penalty()
 
-    def sample(self, x, compute_logprob=False):
-        return self.model.sample(x, compute_logprob)
+# EOF
