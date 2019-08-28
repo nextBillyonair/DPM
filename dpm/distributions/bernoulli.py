Allow categorical and bernoulli params to be learnable, can train MixtureModel with cross entropy, Gaussian Mixture can learn weights on cluster, Fix gumbel softmax log prob with categorical

dpm/distributions/bernoulli.py
@@ -1,17 +1,20 @@
 import torch
 from .distribution import Distribution
 from dpm.utils import eps
+from torch.nn import Parameter
 
 
 class Bernoulli(Distribution):
 
-    def __init__(self, probs=[0.5], learnable=False):
+    def __init__(self, probs=[0.5], learnable=True):
         super().__init__()
         self.n_dims = len(probs)
         if not isinstance(probs, torch.Tensor):
             probs = torch.tensor(probs)
         self.probs = probs.float()
-        self.logits = (self.probs + eps).log()
+        if learnable:
+            self.probs = Parameter(self.probs)
+
 
     def log_prob(self, value):
         q = 1.-self.probs
@@ -32,7 +35,11 @@ class Bernoulli(Distribution):
     def variance(self):
         return self.probs * (1 - self.probs)
 
+    @property
+    def logits(self):
+        return (self.probs + eps).log()
+
     def get_parameters(self):
         if self.n_dims == 1:
-            return {'probs': self.probs.item()}
-        return {'probs': self.probs.numpy()}
+            return {'probs': self.probs.detach().item()}
+        return {'probs': self.probs.detach().numpy()}

dpm/distributions/categorical.py
@@ -1,29 +1,37 @@
 import torch
 from torch import distributions as dists
+from torch.nn import Parameter
 from .distribution import Distribution
 from dpm.utils import eps
 
 # convert to non dist version
-
+# Differentiable log_prob, not differentiable sample
 class Categorical(Distribution):
 
-    def __init__(self, probs=[0.5, 0.5], learnable=False):
+    def __init__(self, probs=[0.5, 0.5], learnable=True):
         super().__init__()
         self.n_dims = len(probs)
         if not isinstance(probs, torch.Tensor):
             probs = torch.tensor(probs)
-        self.probs = probs.float()
-        self.logits = (self.probs + eps).log()
-        self.model = dists.Categorical(probs=probs)
+        self.logits = probs.float().log()
+        if learnable:
+            self.logits = Parameter(self.logits)
 
     def log_prob(self, value):
-        return self.model.log_prob(value).sum(-1)
+        model = dists.Categorical(probs=self.probs)
+        return model.log_prob(value).sum(-1)
 
     def sample(self, batch_size):
-        return self.model.sample((batch_size, 1))
+        model = dists.Categorical(probs=self.probs)
+        return model.sample((batch_size, 1))
 
     def entropy(self):
-        return self.model.entropy()
+        model = dists.Categorical(probs=self.probs)
+        return model.entropy()
+
+    @property
+    def probs(self):
+        return self.logits.softmax(dim=-1)
 
     def get_parameters(self):
-        return {'probs': self.probs.numpy()}
+        return {'probs': self.probs.detach().numpy()}

dpm/distributions/gumbel_softmax.py
@@ -5,6 +5,7 @@ from torch.nn.functional import softplus
 import numpy as np
 import math
 from .distribution import Distribution
+from .categorical import Categorical
 from dpm.utils import eps
 
 
@@ -24,8 +25,8 @@ class GumbelSoftmax(Distribution):
 
     def log_prob(self, value):
         if self.hard or True:
-            model = dists.Categorical(probs=self.probs)
-            return model.log_prob(value)
+            model = Categorical(self.probs, learnable=False)
+            return model.log_prob(value.max(dim=1)[1].view(-1, 1))
         # put non hard pdf here
 
     def sample(self, batch_size):

dpm/mixture_models/mixture_model.py
@@ -33,5 +33,5 @@ class MixtureModel(Distribution):
         return torch.sum(cdfs * cat_cdfs, dim=0)
 
     def get_parameters(self):
-        return {'probs': self.categorical.probs.numpy(),
+        return {'probs': self.categorical.probs.detach().numpy(),
                 'models': [model.get_parameters() for model in self.models]}