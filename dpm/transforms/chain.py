8b9252113f79b189a4c35e41dd8c3c564b4b24d5
Bill Watson <billy_watson96@yahoo.com>
2019-09-11 17:02:08 -0400

    Chain transform to aggregate call to multiple succesive transforms

---
+++ dpm/transforms/chain.py
@@ -0,0 +1,33 @@
+from torch.nn import ModuleList
+from .transform import Transform
+
+
+# Try to use this in transformed dist
+class Chain(Transform):
+
+    def __init__(self, transforms):
+        super().__init__()
+        self.chain = ModuleList(transforms)
+
+    def forward(self, x):
+        for transform in self.chain:
+            x = transform.forward(x)
+        return x
+
+    def inverse(self, y):
+        for transform in reversed(self.chain):
+            y = transform.inverse(y)
+        return y
+
+    def log_abs_det_jacobian(self, x, y):
+        prev_value = x
+        log_det = 0.0
+        for transform in reversed(self.chain):
+            value = transform.inverse(prev_value)
+            log_det += transform.log_abs_det_jacobian(value, prev_value)
+            prev_value = value
+        return log_det
+
+    def get_parameters(self):
+        return {'type':'chain',
+                'compnents':[transform.get_parameters() for transform in self.chain]}
